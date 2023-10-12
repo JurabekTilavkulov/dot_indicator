34f13652475e0807525e9455e4daee4351eb6747
JurabekTilavkulov <71831459+JurabekTilavkulov@users.noreply.github.com>
2023-10-13 00:21:45 +0500

    Ish nihoyasiga yetdi

--- lib/model.dart
+++
@@ -1,8 +0,0 @@
-class ModelClass {
-  String _orqaFon;
-  String _odam;
-  String _kattaYozuv;
-  String _kichikYozuv;
-
-  ModelClass(this._orqaFon, this._odam, this._kattaYozuv, this._kichikYozuv);
-}
